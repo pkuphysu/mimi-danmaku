83f316bc73d124562bbf7b3d298459fcf8d7161a
StevenJoeZhang <1119186082@qq.com>
2018-05-10 12:40:31 +0800

    create files

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mimi Danmaku Client
+
+A Websocket danmaku client based on Node.js and Electron.  
+You may need to install some dependency packages using npm.
+
+## To Use
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+```bash
+# Clone this repository
+git clone https://github.com/stevenjoezhang/mimi-danmaku-client
+# Go into the repository
+cd mimi-danmaku-client
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
+
+## Credits
+* [Mimi](http://zsq.im) Developer of this project.
+
+## License
+Released under the GNU General Public License v3  
+http://www.gnu.org/licenses/gpl-3.0.html
+
---
+++ main.js
@@ -0,0 +1,68 @@
+const electron = require("electron")
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require("path")
+const url = require("url")
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let panelWindow
+
+function createPanel() {
+
+	panelWindow = new BrowserWindow({
+		width: 400,
+		height: electron.screen.getPrimaryDisplay().workAreaSize.height,
+		resizable: false,
+		fullscreenable: false,
+		title: "Mimi Danmaku Panel"
+	})
+
+	panelWindow.loadURL(url.format({
+		pathname: path.join(__dirname, "panel.html"),
+		protocol: "file:",
+		slashes: true
+	}))
+
+	panelWindow.setPosition(0, 0, true);
+
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
+
+	// Emitted when the window is closed.
+	panelWindow.on("closed", function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		panelWindow = null
+	})
+}
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createPanel)
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit()
+	}
+})
+
+app.on("activate", function () {
+	// On OS X it"s common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (panelWindow === null) {
+		createPanel()
+	}
+})
+
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
