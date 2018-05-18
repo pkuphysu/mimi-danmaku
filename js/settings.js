931ac1d1119d298e73653101f4bdcff452da36de
StevenJoeZhang <1119186082@qq.com>
2018-05-19 01:54:53 +0800

    Update files

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,49 @@
+const electron = require("electron").remote;
+const ipcRenderer = require("electron").ipcRenderer;
+const BrowserWindow = electron.BrowserWindow;
+const fs = require("fs");
+var server = "wss://localhost:9005",
+	rule = "",
+	channel = "default";
+
+fs.readFile("config.json", function(err, data) {
+	if (err) throw err;
+	var result = JSON.parse(data.toString());
+	server = result.server || server;
+	rule = result.rule || rule;
+	channel = result.channel || channel;
+	$("#rule").val(rule);
+	$("#channel").val(channel);
+});
+
+var mainWindow = null,
+	currentChannel = "default",
+	outputArray = new Array(),
+	allowArray = new Array(),
+	denyArray = new Array();
+
+var helpArray = [
+	["弹幕窗口将在全屏幕上浮动显示", "弹幕窗口将以窗口形式显示"],
+	["窗口将始终处于屏幕顶层，即使存在其他全屏应用", "使用其他全屏应用时，弹幕窗口可能不会置顶"],
+	["弹幕窗口背景透明", "弹幕窗口使用系统预设背景"],
+	["默认允许所有弹幕", "默认禁止所有弹幕", "弹幕按照规则自动过滤"]
+];
+var options = [0, 0, 0, 0];
+
+function getChannel() {
+	var reg = new RegExp(/[\x00-\xff]+/g);
+	var channel =  $("#channel").val() || "default";
+	if (!reg.test(channel)) channel = "default";
+	return channel;
+}
+
+window.addEventListener("beforeunload", e => {
+	if (mainWindow) {
+		e.returnValue = false;
+		alert("弹幕窗口开启时无法退出控制面板");
+	}
+});
+
 for (var i = 0; i < 4; i++) { //$(".btn-group").length
 	for (var j = 0; j < $(".btn-group").eq(i).find("button").length; j++) {
 		var target = $(".btn-group").eq(i).find("button").eq(j);
