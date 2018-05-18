027913e86b3e649141e90ec791779f4172889975
StevenJoeZhang <1119186082@qq.com>
2018-05-18 20:21:59 +0800

    new feature

--- js/wsconnect.js
+++ js/wsconnect.js
@@ -1,31 +1,36 @@
 var ws = null;
 function wsinit(channel) {
-	ws = new WebSocket("ws://localhost:9000", headers = channel);
+	ws = new WebSocket("ws://localhost:9000", headers = "danmaku" + channel);
 
 	ws.onopen = function() {
 		alert("系统消息：建立连接成功");
-	};
+	}
 
 	ws.onmessage = function(event) {
 		var msg = JSON.parse(event.data);
 		if (msg.type != "user") return;
 		var messageArray = msg.content.split("|");
-		output[index] = msg;
-		$("tbody").append('<tr>\
+		var index = outputArray.length;
+		$("tbody").prepend('<tr id="' + index + '">\
 					<td>' + messageArray[0] + '</td>\
-					<td style="max-width: 80px;">\
+					<td style="max-width: 90px;">\
 						<div class="form-inline">\
 							<button type="button" class="btn btn-success" onclick="allow(' + index + ')">通过</button>\
 							<button type="button" class="btn btn-danger" onclick="deny(' + index + ')">禁止</button>\
 						</div>\
 					</td>\
 				</tr>');
-		index++;
-	};
+		outputArray.push(msg);
+		if (options[3] == 0) allow(index);
+		else if (options[3] == 1) deny(index);
+		else {
+			;
+		}
+	}
 
 	ws.onerror = function() {
 		alert("系统消息：连接失败，请手动关闭窗口并稍后再试");
-	};
+	}
 
 	if (mainWindow) mainWindow.webContents.send("setchannel", channel);
 }
