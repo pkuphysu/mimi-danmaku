edf395065e9c2fe40717977b35778fda87ee9be3
StevenJoeZhang <1119186082@qq.com>
2018-08-29 22:28:32 +0800

    Change priority

--- js/settings.js
+++ js/settings.js
@@ -26,7 +26,7 @@ var helpArray = [
 	["弹幕窗口将在全屏幕上浮动显示", "弹幕窗口将以窗口形式显示"],
 	["窗口将始终处于屏幕顶层，即使存在其他全屏应用", "使用其他全屏应用时，弹幕窗口可能不会置顶"],
 	["弹幕窗口背景透明", "弹幕窗口使用系统预设背景"],
-	["默认允许所有弹幕", "默认禁止所有弹幕", "弹幕按照规则自动过滤"]
+	["弹幕按照规则自动过滤", "默认允许所有弹幕", "默认禁止所有弹幕"]
 ];
 var options = [0, 0, 0, 0];
 
@@ -90,5 +90,5 @@ function panelSubmit(event) {
 }
 
 function about() {
-	alert("We are using Node.js" + process.versions.node + ", Chromium" + process.versions.chrome + ", and Electron" + process.versions.electron + ". Powered by Mimi.");
+	alert("Mimi Danmaku Ver 1.0.1\n\nWe are using Node.js " + process.versions.node + ", Chromium " + process.versions.chrome + ", and Electron " + process.versions.electron + ". Powered by Mimi.");
 }
