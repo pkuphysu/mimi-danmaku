e9b2031092ac433426e46f7da802990fd0e50f03
StevenJoeZhang <1119186082@qq.com>
2018-11-17 00:09:26 +0800

    new feature

--- js/settings.js
+++ js/settings.js
@@ -26,9 +26,10 @@ var helpArray = [
 	["弹幕窗口将在全屏幕上浮动显示", "弹幕窗口将以窗口形式显示"],
 	["窗口将始终处于屏幕顶层，即使存在其他全屏应用", "使用其他全屏应用时，弹幕窗口可能不会置顶"],
 	["弹幕窗口背景透明", "弹幕窗口使用系统预设背景"],
-	["弹幕按照规则自动过滤", "默认允许所有弹幕", "默认禁止所有弹幕"]
+	["弹幕按照规则自动过滤", "默认允许所有弹幕", "默认禁止所有弹幕"],
+	["含有屏蔽词的弹幕直接禁止", "替换屏蔽词为*"]
 ];
-var options = [0, 0, 0, 0];
+var options = [0, 0, 0, 0, 0];
 
 function getChannel() {
 	var reg = new RegExp(/[\x00-\xff]+/g);
@@ -44,7 +45,7 @@ window.addEventListener("beforeunload", e => {
 	}
 });
 
-for (var i = 0; i < 4; i++) { //$(".btn-group").length
+for (var i = 0; i < 5; i++) { //$(".btn-group").length
 	for (var j = 0; j < $(".btn-group").eq(i).find("button").length; j++) {
 		var target = $(".btn-group").eq(i).find("button").eq(j);
 		target.attr("i", i);
